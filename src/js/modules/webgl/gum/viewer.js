7ddb67a99fd90b3d12b5b0b2953a2130b74d8c27
Dmitry <dmitriy@Bono-Dmitry-Smirnov-Vladimirovich.local>
2020-08-27 18:58:09 +0300

    fix

--- src/js/modules/webgl/gum/viewer.js
+++ src/js/modules/webgl/gum/viewer.js
@@ -1,6 +1,18 @@
 import * as THREE from 'three'
+import { EffectComposer } from 'three/examples/jsm/postprocessing/EffectComposer.js'
+import { GlitchPass } from 'three/examples/jsm/postprocessing/GlitchPass.js'
+import { BokehPass } from 'three/examples/jsm/postprocessing/BokehPass.js';
+import { UnrealBloomPass } from 'three/examples/jsm/postprocessing/UnrealBloomPass.js';
+import { RenderPass } from 'three/examples/jsm/postprocessing/RenderPass.js'
 import Stats from 'three/examples/jsm/libs/stats.module'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
+
+const params = {
+  exposure: 0.1,
+  bloomStrength: 0.2,
+  bloomThreshold: 0,
+  bloomRadius: 0
+};
 
 const Viewer = class {
   constructor (data) {
@@ -9,19 +21,29 @@ const Viewer = class {
     this.scene = new THREE.Scene()
     this.renderData = data.renderData ? data.renderData : {}
     this.renderer = new THREE.WebGLRenderer(this.renderData)
+
+
     this.renderer.outputEncoding = THREE.sRGBEncoding
-    this.renderer.setPixelRatio(1.5)
+    this.renderer.setPixelRatio(2)
     this.container.appendChild(this.renderer.domElement)
 
     this.setSize()
     this.camera = new THREE.PerspectiveCamera(45, this.width / this.height, 1, 1000)
-
     this.scene.add(this.camera)
     this.camera.position.y = 2
     this.camera.position.z = -5
     this.stats = new Stats()
     this.container.appendChild(this.stats.dom)
     this.controls = new OrbitControls(this.camera, this.renderer.domElement)
+
+    this.composer = new EffectComposer(this.renderer)
+    this.renderPass = new RenderPass(this.scene, this.camera)
+    this.composer.addPass(this.renderPass)
+    this.bloomPass = new UnrealBloomPass( new THREE.Vector2( window.innerWidth, window.innerHeight ), 1.5, 0.4, 0.85 )
+    this.bloomPass.threshold = params.bloomThreshold
+    this.bloomPass.strength = params.bloomStrength
+    this.bloomPass.radius = params.bloomRadius
+    this.composer.addPass(this.bloomPass)
     window.addEventListener('resize', function () {
       that.onResize()
     })
@@ -48,7 +70,8 @@ const Viewer = class {
 
   update () {
     this.controls.update()
-    this.renderer.render(this.scene, this.camera)
+    // this.renderer.render(this.scene, this.camera)
+    this.composer.render()
     this.stats.update()
   }
 }
