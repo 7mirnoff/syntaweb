44e0513ef0a12ded883c3f590a413170668e0d81
Dmitry <dmitriy@Bono-Dmitry-Smirnov-Vladimirovich.local>
2020-08-28 18:41:19 +0300

    fix

--- src/js/modules/scene/postprocessing.js
+++ src/js/modules/scene/postprocessing.js
@@ -4,25 +4,25 @@ import { BokehPass } from 'three/examples/jsm/postprocessing/BokehPass.js'
 import { UnrealBloomPass } from 'three/examples/jsm/postprocessing/UnrealBloomPass.js'
 
 const postprocessing = (g) => {
-  const params = {
+  const paramsBloom = {
     exposure: 0.1,
     bloomStrength: 0.7,
     bloomThreshold: 0,
-    bloomRadius: 0
+    bloomRadius: 0.1
   }
 
   const bloomPass = new UnrealBloomPass(new THREE.Vector2(window.innerWidth, window.innerHeight), 1.5, 0.4, 0.85)
-  bloomPass.threshold = params.bloomThreshold
-  bloomPass.strength = params.bloomStrength
-  bloomPass.radius = params.bloomRadius
+  bloomPass.threshold = paramsBloom.bloomThreshold
+  bloomPass.strength = paramsBloom.bloomStrength
+  bloomPass.radius = paramsBloom.bloomRadius
   g.v.addEffect(bloomPass)
 
   const glitch = new GlitchPass()
-  g.v.addEffect(glitch)
+  // g.v.addEffect(glitch)
 
   const bokehPass = new BokehPass(g.v.scene, g.v.camera, {
-    focus: 1.0,
-    aperture: 0.00001,
+    focus: 20.0,
+    aperture: 0.0001,
     maxblur: 0.001,
 
     width: window.innerWidth,
